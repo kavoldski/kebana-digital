5d4838f20be758a42e2badfb6cdacf8d65842165
kavoldski <kavoldski@gmail.com>
2026-05-21 16:43:39 +0800

    chore: clean up obsolete scratch files and redundant tests

--- scratch/list_users.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../bootstrap.php';
-$db = App\Core\Database::getInstance()->getConnection();
-$res = $db->query("SELECT user_id, username, role, email FROM tbl_user");
-$users = [];
-while ($row = $res->fetch_assoc()) {
-    $users[] = $row;
-}
-echo json_encode($users);
